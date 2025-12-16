9ff1dce14e3f70a31662a131ca0fea6f4a1ca31b
mkhshohud99 <mkhshohud1999@gmail.com>
2025-12-17 01:36:39 +0600

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+
+require ('dotenv').config()
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json);
