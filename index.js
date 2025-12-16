28bf2864a2f595971a60c6982f9231b48110eb95
mkhshohud99 <mkhshohud1999@gmail.com>
2025-12-17 03:54:40 +0600

    mongoDB Added

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 const express = require('express');
 const cors = require('cors');
 
-require ('dotenv').config()
+require('dotenv').config()
 const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sctb0kd.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send("Hello, Assignment 11")
+})
+
+app.listen(port, () => {
+    console.log(port);
+
+})
